a75fff56afbffaf7ee7a573118b299c557a4401e
Anastassia Titova <atitova2021@gmail.com>
2021-09-15 14:08:02 -0400

    09/15/2021 update

--- blog-capstone/src/main/java/com/company/blogcapstone/controller/AuthorController.java
+++ blog-capstone/src/main/java/com/company/blogcapstone/controller/AuthorController.java
@@ -2,25 +2,29 @@ package com.company.blogcapstone.controller;
 
 import com.company.blogcapstone.dao.AuthorRepository;
 import com.company.blogcapstone.dto.Author;
+import com.company.blogcapstone.dto.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/author")
 public class AuthorController {
+
     @Autowired
     AuthorRepository authorRepo;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Author addAuthor(@RequestBody Author newAuthor){
+    public Author addAuthor(@RequestBody Author newAuthor) {
         Author savedAuthor = authorRepo.save(newAuthor);
         final int authorId = savedAuthor.getId();
-        if(newAuthor.getPosts() != null){
+        if (newAuthor.getPosts() != null) {
             newAuthor.getPosts().forEach(post -> post.setAuthorId(authorId));
         }
         savedAuthor = authorRepo.save(newAuthor);
@@ -29,15 +33,15 @@ public class AuthorController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<Author> getAllAuthors(){
+    public List<Author> getAllAuthors() {
         return authorRepo.findAll();
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Author getAuthorById(@PathVariable int id){
+    public Author getAuthorById(@PathVariable int id) {
         Optional<Author> author = authorRepo.findById(id);
-        if(author.isPresent()){
+        if (author.isPresent()) {
             return author.get();
         } else {
             return null;
@@ -46,7 +50,28 @@ public class AuthorController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteAuthor(@PathVariable int id){
+    public void deleteAuthor(@PathVariable int id) {
         authorRepo.deleteById(id);
+    }
+
+    @GetMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    public ModelAndView loginAuthor() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/login");
+        return modelAndView;
+    }
+
+    @PostMapping("loginAuthor")
+    public ModelAndView addTeacher(HttpServletRequest request) {
+        String email = request.getParameter("emailLogin");
+        String password = request.getParameter("passwordLogin");
+        
+        List<Author> author = getAllAuthors();
+        
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/post");
+        
+        return modelAndView;
     }
 }
--- blog-capstone/src/main/java/com/company/blogcapstone/controller/PostController.java
+++ blog-capstone/src/main/java/com/company/blogcapstone/controller/PostController.java
@@ -8,36 +8,43 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/post")
+@RequestMapping("/")
 public class PostController {
+
     @Autowired
     PostRepository postRepo;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Post addPost(@RequestBody Post newPost){
+    public Post addPost(@RequestBody Post newPost) {
         Post savedPost = postRepo.save(newPost);
         final int postId = savedPost.getId();
-        if(newPost.getCategories() != null){
+        if (newPost.getCategories() != null) {
             newPost.getCategories().forEach(category -> category.getPosts().add(newPost));
         }
         savedPost = postRepo.save(newPost);
         return savedPost;
     }
 
-    @GetMapping
+    @GetMapping()
     @ResponseStatus(HttpStatus.OK)
-    public List<Post> getAllPost(){
-        return postRepo.findAll();
+    public ModelAndView getAllPost(Model model) {
+        List<Post> posts = postRepo.findAll();
+        model.addAllAttributes(posts);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("post");
+        return modelAndView;
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Post getPostById(@PathVariable int id){
+    public Post getPostById(@PathVariable int id) {
         Optional<Post> post = postRepo.findById(id);
-        if(post.isPresent()){
+        if (post.isPresent()) {
             return post.get();
         } else {
             return null;
@@ -46,13 +53,13 @@ public class PostController {
 
     @PutMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updatePost(@RequestBody Post updatedPost){
+    public void updatePost(@RequestBody Post updatedPost) {
         postRepo.save(updatedPost);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deletePost(@PathVariable int id){
+    public void deletePost(@PathVariable int id) {
         postRepo.deleteById(id);
     }
 }
