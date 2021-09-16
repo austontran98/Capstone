acec131e58b913ac7a46c758b286842922e26ce0
Reynaldo <67420789+Reydg4@users.noreply.github.com>
2021-09-16 10:47:36 -0400

    Add files via upload
    My bad for not being able to push 🥲

---
+++ blog-capstone1/src/main/java/com/company/blogcapstone/controller/CategoryController.java
@@ -0,0 +1,28 @@
+package com.company.blogcapstone.controller;
+
+import com.company.blogcapstone.dao.CategoryRepository;
+import com.company.blogcapstone.dto.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+    @Autowired
+    CategoryRepository categoryRepo;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Category addCategory(@RequestBody Category newCategory){
+        return categoryRepo.save(newCategory);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Category> getAllCategories(){
+        return categoryRepo.findAll();
+    }
+}
